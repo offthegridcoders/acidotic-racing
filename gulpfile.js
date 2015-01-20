10aa0b76cd3738c94eef59121b92225817b54737
Stephen <steveyoung359@gmail.com>
2015-01-19 22:05:49 -0600

    added dist fold and minify

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 var gulp = require('gulp');
 var scsslint = require('gulp-scss-lint');
 var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var minifyCSS = require('gulp-minify-css');
 
 var paths = {
   images: 'src/assets/**/*',
-  scss: 'src/scss/**/*'
+  scss: 'src/scss/**/*',
+  css: 'src/css/**/*.css'
 };
 
 gulp.task('lint', function() {
@@ -18,6 +21,18 @@ gulp.task('sass', function () {
         .pipe(gulp.dest('./src/css'));
 });
 
+gulp.task('compress', function() {
+  gulp.src('src/js/**/*')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/'))
+});
+
+gulp.task('minify-css', function() {
+  gulp.src(paths.css)
+    .pipe(minifyCSS({keepBreaks:true}))
+    .pipe(gulp.dest('./dist/'))
+});
+
 // Rerun the task when a file changes
 gulp.task('watch', function() {
   gulp.watch(paths.scss, ['lint']);
@@ -26,3 +41,6 @@ gulp.task('watch', function() {
 
 // The default task (called when you run `gulp` from cli)
 gulp.task('default', ['lint', 'sass','watch']);
+
+
+gulp.task('build', ['lint', 'sass','compress', 'minify-css']);
