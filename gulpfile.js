e77b11cfe8618cd0cf8b4e023bf6edb80d350a75
Stephen <steveyoung359@gmail.com>
2015-01-19 22:21:43 -0600

    added more minification

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,16 @@ var scsslint = require('gulp-scss-lint');
 var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
 var minifyCSS = require('gulp-minify-css');
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
+var minifyHTML = require('gulp-minify-html');
 
 var paths = {
   images: 'src/assets/**/*',
   scss: 'src/scss/**/*',
-  css: 'src/css/**/*.css'
+  assets: 'src/assets/**/*',
+  css: 'src/css/**/*.css',
+  html: 'src/**/*.html'
 };
 
 gulp.task('lint', function() {
@@ -24,12 +29,29 @@ gulp.task('sass', function () {
 gulp.task('compress', function() {
   gulp.src('src/js/**/*')
     .pipe(uglify())
-    .pipe(gulp.dest('./dist/'))
+    .pipe(gulp.dest('./dist/js'))
 });
 
 gulp.task('minify-css', function() {
   gulp.src(paths.css)
     .pipe(minifyCSS({keepBreaks:true}))
+    .pipe(gulp.dest('./dist/css'))
+});
+
+gulp.task('image-minify', function () {
+  return gulp.src(paths.assets)
+    .pipe(imagemin({
+        progressive: true,
+        svgoPlugins: [{removeViewBox: false}],
+        use: [pngquant()]
+    }))
+    .pipe(gulp.dest('./dist/assets'))
+});
+
+gulp.task('minify-html', function() {
+  var opts = {comments:true,spare:true};
+  gulp.src(paths.html)
+    .pipe(minifyHTML(opts))
     .pipe(gulp.dest('./dist/'))
 });
 
@@ -42,5 +64,5 @@ gulp.task('watch', function() {
 // The default task (called when you run `gulp` from cli)
 gulp.task('default', ['lint', 'sass','watch']);
 
-
-gulp.task('build', ['lint', 'sass','compress', 'minify-css']);
+// Makes Distribution folder with all files minified
+gulp.task('build', ['lint', 'sass','compress', 'minify-css', 'image-minify', 'minify-html']);
