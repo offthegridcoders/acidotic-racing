433b782dd71e79b3bfea2a75ea2ebf036dfe0ed3
Stephen <steveyoung359@gmail.com>
2015-01-21 17:35:10 -0600

    changes

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,8 +1,8 @@
 function setNavBar() {
   console.log('ScrollTop: ' + $(window).scrollTop());
-  console.log('Header Height: ' + $('header').css('height'));
+  console.log('Header Height: ' + parseInt($('header').css('height'), 10));
 
-  if ($(window).scrollTop() < $('header').css('height')) {
+  if ($(window).scrollTop() < parseInt($('header').css('height'), 10)) {
     console.log('bang');
   } else {
     console.log('boom');
