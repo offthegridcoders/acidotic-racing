7b5b94f7f665ea8e4693b4a233dd02657ddc9c8c
Stephen <steveyoung359@gmail.com>
2015-01-21 17:24:21 -0600

    changes

---
+++ src/js/parallax.js
@@ -0,0 +1,19 @@
+function checkHeaderPosition() {
+  if ($(window).scrollTop() <= 0) {
+    $('.parallax-header').css('top', '0px');
+  }
+};
+
+function parallax(){
+  var scrollPosition = $(window).scrollTop();
+  var speed = -1.3;
+  var newTopPosition = scrollPosition * speed;
+  if ($(window).scrollTop() > 0) {
+    $('header').css('top', newTopPosition + 'px');
+  }
+  checkHeaderPosition();
+}
+
+$(window).scroll(function(e){
+  parallax();
+});
