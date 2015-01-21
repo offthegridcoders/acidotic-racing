0acbf05b4e966fa20052d0e5eab8e2000e9c4264
Stephen <steveyoung359@gmail.com>
2015-01-21 17:59:02 -0600

    changes

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -2,13 +2,13 @@ function setNavBar() {
   if ($(window).scrollTop() > (parseInt($('header').css('height'), 10) - 100)) {
     $('nav').css('position', 'fixed');
     $('nav').css('top', '0px');
-    $('nav').hover(function() {
-      $(this).css('background','rgba(0, 0, 0, .1')
-    });
   } else {
     $('nav').css('position', 'absolute');
-    $('nav').hover(function() {
+    $('nav').mouseover(function() {
       $(this).css('background','rgba(0, 0, 0, .8')
+    });
+    $('nav').mouseout(function() {
+      $(this).css('background','rgba(0, 0, 0, .1')
     });
   }
 };
