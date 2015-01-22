9403e3410ae430f51f0d1d42c8257a35679625ff
Stephen <steveyoung359@gmail.com>
2015-01-21 23:31:02 -0600

    successful gulp and gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -20,8 +20,10 @@ var paths = {
   distHTML: 'dist/**/*.html'
 };
 
+var pathsCopy;
+
 // The default task (called when you run `gulp` from cli)
-gulp.task('default', ['lint', 'sass','watch']);
+/*gulp.task('default', ['lint', 'sass','watch']);
 
 gulp.task('lint', function() {
   gulp.src(paths.scss)
@@ -32,21 +34,30 @@ gulp.task('sass', function () {
   gulp.src(paths.scss)
     .pipe(sass())
     .pipe(gulp.dest('./src/css'));
-});
+});*/
 
 // Makes Distribution folder with all files minified
-gulp.task('build', ['clean', 'useref', 'img-min'], function() {
+gulp.task('default', ['sass', 'clean', 'useref', 'img-copy'], function() {
   return gulp.src(paths.distHTML)
     .pipe(fileinclude())
     .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('clean', function () {
-  return gulp.src('dist', {read: false})
-    .pipe(clean());
+// Makes Distribution folder with all files minified
+gulp.task('build', ['sass', 'clean', 'useref', 'img-min'], function() {
+  return gulp.src(paths.distHTML)
+    .pipe(fileinclude())
+    .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('useref', ['clean'], function () {
+
+gulp.task('sass', ['lint'], function () {
+  gulp.src(paths.scss)
+    .pipe(sass())
+    .pipe(gulp.dest('./src/css'));
+});
+
+gulp.task('useref', ['fileinclude'], function () {
   var assets = useref.assets();
   gulp.src(paths.html)
     .pipe(assets)
@@ -67,15 +78,25 @@ gulp.task('img-min', ['clean'], function () {
     .pipe(gulp.dest('./dist/assets'));
 });
 
-gulp.task('fileinclude', function() {
+gulp.task('img-copy', ['clean'], function() {
+  gulp.src(paths.assets)
+  // Perform minification tasks, etc here
+  .pipe(gulp.dest('./dist/assets'));
+})
+
+gulp.task('lint', function() {
+  gulp.src(paths.scss)
+    .pipe(scsslint());
+});
+
+gulp.task('clean', ['sass'], function () {
+  return gulp.src('dist', {read: false})
+    .pipe(clean());
+});
+
+gulp.task('fileinclude', ['clean'], function() {
   return gulp.src(paths.distHTML)
     .pipe(fileinclude())
     .pipe(gulp.dest('./dist'));
 });
 
-// Rerun the task when a file changes
-gulp.task('watch', function() {
-  gulp.watch(paths.scss, ['fileinclude']);
-  gulp.watch(paths.scss, ['lint']);
-  gulp.watch(paths.scss, ['sass']);
-});
