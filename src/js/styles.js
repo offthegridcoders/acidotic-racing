e506ace39044b99e406f70b77f7561f7fe19383c
Stephen <steveyoung359@gmail.com>
2015-01-22 15:09:17 -0600

    updated

--- src/js/styles.js
+++ src/js/styles.js
@@ -1,4 +1,5 @@
 function setNavBar(elm) {
+  // NAV styling for pages with a header
   if (headerIsOffScreen()) {
     $(elm).css('position', 'fixed');
     $(elm).css('top', '0px');
@@ -6,5 +7,13 @@ function setNavBar(elm) {
   } else {
     $(elm).css('position', 'absolute');
     $(elm).css('background','rgba(0, 0, 0, 0');
+  }
+
+  // NAV styling for pages without header
+  if ($('header')) {
+    console.log('header exists')
+  } else {
+    $('nav').css('position', 'absolute');
+    $('nav').css('background','rgba(0, 0, 0, 0');
   }
 };
