1d920e23f3f3b9ed91c5ec872a46650b06af13b8
Stephen <steveyoung359@gmail.com>
2015-01-21 18:02:53 -0600

    changes

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -2,17 +2,7 @@ function setNavBar() {
   if ($(window).scrollTop() > (parseInt($('header').css('height'), 10) - 100)) {
     $('nav').css('position', 'fixed');
     $('nav').css('top', '0px');
-    $('nav').mouseover(function() {
-      $(this).css('background','rgba(0, 0, 0, .1')
-    });
   } else {
     $('nav').css('position', 'absolute');
-    $('nav').mouseover(function() {
-      $(this).css('background','rgba(0, 0, 0, .8')
-    });
-    $('nav').mouseout(function() {
-      $(this).css('background','rgba(0, 0, 0, .1')
-    });
   }
 };
-
---
+++ src/js/styles.js
@@ -0,0 +1,11 @@
+$('nav').mouseover(function() {
+  if ($(window).scrollTop() > (parseInt($('header').css('height'), 10) - 100) {
+    $(this).css('background','rgba(0, 0, 0, .8');
+  } else {
+    $(this).css('background','rgba(0, 0, 0, .1');
+  }
+});
+
+$('nav').mouseout(function() {
+  $(this).css('background','rgba(0, 0, 0, .1');
+});
